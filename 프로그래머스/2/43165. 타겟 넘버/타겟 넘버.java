b0dcc5a7a2e2891bba619b43f231d689026643f4
junw00 <127822736+junw00@users.noreply.github.com>
2025-03-15 16:09:14 +0900

    [level 2] Title: 타겟 넘버, Time: 136.85 ms, Memory: 158 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,15 +1,35 @@
+import java.util.*;
 class Solution {
     int answer = 0;
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0, 0);
-        return answer;
+
+        int cnt = 0;
+        int[][] result = new int[numbers.length][(int) Math.pow(2, numbers.length)];
+        
+        result[0][0] += numbers[0];
+        result[0][1] -= numbers[0];
+            
+        for(int i = 1; i < result.length; i++) {
+            int idx = 0;
+            
+            for(int j = 0; j < (int) Math.pow(2, i + 1); j++) { 
+                
+                
+                if(j % 2 == 0) {
+                    result[i][j] = result[i - 1][idx] + numbers[i];    
+                }else {
+                    result[i][j] = result[i - 1][idx++] - numbers[i]; 
+                }
+                
+                if(i == result.length - 1 && result[i][j] == target) {
+                    cnt++;
+                }
+                
+            }
+        }
+        
+        return cnt;
     }
     
-    public void dfs(int[] numbers, int target, int idx, int cur) {
-        if(cur == target && idx == numbers.length) answer++;
-        if(idx == numbers.length) return;
-        
-        dfs(numbers, target, idx + 1, cur + numbers[idx]);
-        dfs(numbers, target, idx + 1, cur - numbers[idx]);
-    }
+    
 }
