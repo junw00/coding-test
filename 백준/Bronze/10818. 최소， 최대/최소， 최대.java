f924cc414b5cced13685cc6ad52d7b2b6f73bcff
junw00 <127822736+junw00@users.noreply.github.com>
2025-04-12 16:55:59 +0900

    [Bronze III] Title: 최소, 최대, Time: 488 ms, Memory: 110624 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -5,8 +5,12 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         br.readLine();
         int[] arr =Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int max = Arrays.stream(arr).max().getAsInt();
-        int min = Arrays.stream(arr).min().getAsInt();
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i : arr) {
+            max = Math.max(max, i);
+            min = Math.min(min, i);
+        }
         System.out.println(min + " " + max);
     }
 }
