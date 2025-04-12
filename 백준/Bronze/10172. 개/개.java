49eeaab3860421caeaaaa88f45c0b07126f61fcf
junw00 <127822736+junw00@users.noreply.github.com>
2025-04-12 14:29:11 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
+/**        |\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+*/
+    }
+}
